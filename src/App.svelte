511080511508bc1db75217f6f0f95f76c028727c
IoyoCode <me@ioyo.dev>
2023-02-26 16:17:45 +0000

    updates

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,15 @@
 <script>
   import Header from "./otc/Header.svelte";
   import Note from "./lib/Note.svelte";
-  import Fab from "./lib/Fab.svelte";
 
   let notes = []
+  let morePages = true
+  let page = 0
+  let maxPage = 0
 
   // TODO grab notes from backend
-    function getNotes() {
+    function getNotes(pageNumber) {
+        console.log(pageNumber)
         notes = [
             {
                 name: "d.jar docs",
@@ -22,37 +25,229 @@
                 name: "i cant think of placeholder names",
                 icon: "bookmark",
                 id: "note-1"
+            },
+            {
+                name: "i cant think of placeholder names",
+                icon: "bookmark",
+                id: "note-1"
+            },
+            {
+                name: "i cant think of placeholder names",
+                icon: "bookmark",
+                id: "note-1"
+            },
+            {
+                name: "d.jar docs",
+                icon: "bookmark",
+                id: "note-1"
+            },
+            {
+                name: "memolink",
+                icon: "bookmark",
+                id: "note-1"
+            },
+            {
+                name: "i cant think of placeholder names",
+                icon: "bookmark",
+                id: "note-1"
+            },
+            {
+                name: "i cant think of placeholder names",
+                icon: "bookmark",
+                id: "note-1"
+            },
+            {
+                name: "i cant think of placeholder names",
+                icon: "bookmark",
+                id: "note-1"
             }
         ]
+
+        page = 1;
+        morePages = true;
+        maxPage = 4;
     }
 
-    getNotes()
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    async function loadNotes() {
+        await sleep(2000);
+        // check if there is a page number in the url
+        const urlParams = new URLSearchParams(window.location.search);
+        const loadPage = urlParams.get('page');
+
+        getNotes(loadPage == null ? 1 : loadPage);
+        // @ts-ignore
+        page = loadPage == null ? 1 : loadPage;
+    }
+
+    function goRight() {
+        let newpage = page + 1; 
+        if (newpage > maxPage) {
+            newpage = maxPage;
+        }
+        notes = []
+        getNotes(newpage);
+        page = newpage;
+        
+        history.pushState(null, null, `?page=${page}`);
+    }
+
+    function goLeft() {
+        let newpage = page - 1; 
+        if (newpage < 1) {
+            newpage = 1;
+        }
+        notes = []
+        getNotes(newpage);
+        page = newpage;
+
+        history.pushState(null, null, `?page=${page}`);
+    }
+
+    loadNotes();
 
 </script>
 
+<div class="fullpage">
 <main>
     <Header name = "Seailz" />
     <div class="notes">
+        <!-- When not loaded yet, (or the notes array is empty), then put a loading icon -->
+
+        {#if notes.length == 0}
+                <div class="note_list_el">
+                    <!-- Center under middle of header -->
+                    <div class="headercenter">
+                        <div class="lds-ripple"><div></div><div></div></div>
+                    </div>
+
+                    <style>
+                        .headercenter {
+                            position: absolute;
+                            top: 50%;
+                            left: 50%;
+                            transform: translate(-50%, -50%);
+                        }
+                    </style>
+                </div>
+        {/if}
+
         {#each notes as note}
             <div class="note_list_el">
                 <Note name="{note.name}" />
             </div>
         {/each}
+
+        {#if morePages && notes.length > 0}
+        <div class="page_switcher no_select">
+            <button on:click={goLeft} class="ps_arrow ps_arrow_left">
+                <p class="material-icons ps_icon">chevron_left</p>
+            </button>
+            <p class="page_status no_select">{page} / {maxPage}</p>
+            <button on:click={goRight}  class="ps_arrow ps_arrow_right">
+                <p class="material-icons ps_icon">chevron_right</p>
+            </button>
+        </div>
+        {/if}
     </div>
-    <Fab name="Create" icon="add" />
 </main>
+</div>
 
 <style>
+
+.no_select {
+    -webkit-user-select: none; /* Safari */
+  -ms-user-select: none; /* IE 10 and IE 11 */
+  user-select: none; /* Standard syntax */
+}
+
+.ps_arrow {
+    background: none;
+    border: none;
+    color: white;
+    font-size: 2em;
+    cursor: pointer;
+    outline: none;
+    height: 10px;
+    margin: 0px!important;
+    display: inline;
+    position: relative;
+    top: 5px;
+}
+
+.ps_icon {
+    margin-bottom: 0px;
+}
+
+.page_status {
+    color: white;
+    font-size: 1em;
+    font-family: 'Poppins', sans-serif;
+    display: inline;
+    margin-top: 0px;
+}
+
+
+
+.lds-ripple {
+  display: inline-block;
+  position: relative;
+  width: 80px;
+  height: 80px;
+}
+.lds-ripple div {
+  position: absolute;
+  border: 4px solid #fff;
+  opacity: 1;
+  border-radius: 50%;
+  animation: lds-ripple 1s cubic-bezier(0, 0.2, 0.8, 1) infinite;
+}
+.lds-ripple div:nth-child(2) {
+  animation-delay: -0.5s;
+}
+@keyframes lds-ripple {
+  0% {
+    top: 36px;
+    left: 36px;
+    width: 0;
+    height: 0;
+    opacity: 0;
+  }
+  4.9% {
+    top: 36px;
+    left: 36px;
+    width: 0;
+    height: 0;
+    opacity: 0;
+  }
+  5% {
+    top: 36px;
+    left: 36px;
+    width: 0;
+    height: 0;
+    opacity: 1;
+  }
+  100% {
+    top: 0px;
+    left: 0px;
+    width: 72px;
+    height: 72px;
+    opacity: 0;
+  }
+}
+
     main {
         margin-top: 8%;
-        width: 90%;
-        margin: 8% 5%;
+        width: 70%;
+        margin: 8% 12%;
     }
 
-    /* note_list_el selector excluding the first element */
-    .notes > :not(:first-child) {
-        display: inline-block;
-        margin: 10px 10px;
+    /* note_list_el selector excluding the first element of each line */
+    .note_list_el:not(:first-child) {
+        margin-top: 10px;
     }
 
     .note_list_el {
@@ -61,6 +256,17 @@
     }
 
     .notes {
-        width: 70%;
+        /*width: 70%;*/
+    }
+
+    .fullpage {
+        width: 100%;
+        height: 100%;
+        background: linear-gradient(0deg, rgba(224,173,221,1) 0%, rgba(255,204,134,1) 100%);
+        position: absolute;
+        top: 0;
+        left: 0;
+        margin: 0;
+        padding: 0;
     }
 </style>
--- src/otc/Header.svelte
+++ src/otc/Header.svelte
@@ -1,12 +1,13 @@
 <script>
   export let name = "";
+  export let img = "https://picsum.photos/1000/1000"
   let greetings = [
-    "Hey, {name}!",
-    "Hi, {name}!",
-    "Hello, {name}!",
-    "Hiya, {name}!",
-    "Hey there, {name}!",
-    "Howdy, {name}!",
+    "Hey, {name}",
+    "Hi, {name}",
+    "Hello, {name}",
+    "Hiya, {name}",
+    "Hey there, {name}",
+    "Howdy, {name}",
     "How's it going, {name}?",
     "What's up, {name}?",
     "What's new, {name}?",
@@ -15,10 +16,10 @@
   ]
 
   let timeSensitiveGreetings = [
-    "Good morning, {name}!",
-    "Good afternoon, {name}!",
-    "Good evening, {name}!",
-    "Good night, {name}!",
+    "Good morning, {name}",
+    "Good afternoon, {name}",
+    "Good evening, {name}",
+    "Good night, {name}",
   ]
 
     let date = new Date();
@@ -41,26 +42,136 @@
    }
 
    greeting = greeting.replace("{name}", name);
+
+   function newNote() {
+    // make button flash
+    let button = document.getElementsByClassName("button")[0];
+    // @ts-ignore
+    button.style.backgroundColor = "rgba(0,0,0,.5)";
+    setTimeout(() => {
+      // @ts-ignore
+      button.style.backgroundColor = "rgba(0,0,0,.3)";
+    }, 100);
+
+    // open dialog
+   }
 </script>
 
 <!-- svelte-ignore a11y-img-redundant-alt -->
-<div class="userspecific">
+<div class="container">
+  <div class="userspecific">
     <h1>{greeting}</h1>
+</div>
+
+<div class="left_alligned">
+    <button class="new_note button" on:click={newNote}>
+      <p class="material-icons new_note_icon button_icon">add</p>
+    </button>
+    <img class="pfp" src="{img}" alt="User Profile Pic">
+</div>
 </div>
 
 <style>
   @import url("https://fonts.googleapis.com/css2?family=Poppins&display=swap");
 
+  .pfp {
+    width: 80px;
+    height: 80px;
+    border-radius: 100%;
+    margin-left: 30px;
+    box-shadow: rgba(0, 0, 0, 0.11) 0px 0px 10px;
+    transform-origin: center center 0 0 0 0;
+    transition-property: all;
+    transition-duration: 500ms;
+  }
+
+  .pfp:hover {
+    background-color: rgba(0,0,0,.5);
+    cursor: pointer;
+    transform: scale(1.1);
+    box-shadow: rgba(0, 0, 0, 0.405) 0px 0px 90px;
+  }
+
+  .new_note {
+    margin-top: 10px;
+  }
+
+  .left_alligned {
+    display: flex;
+    float: right;
+  }
+
+  .button:hover {
+    background-color: rgba(0,0,0,.5);
+    cursor: pointer;
+    transform: scale(1.1);
+    box-shadow: rgba(0, 0, 0, 0.405) 0px 0px 90px;
+  }
+
+  @media only screen and (max-width: 800px) {
+    .left_alligned {
+    }
+
+
+    .container {
+      display: block;
+    }
+  }
+
   .userspecific {
     display: inline;
+  }
+
+  .container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-top: 8%;
+    width: 70%;
+    margin: 8% 12%;
   }
 
   h1 {
     font-family: "Poppins", sans-serif;
     display: inline;
     color: white;
-    font-size: 60px;
+    font-size: 50px;
     vertical-align: middle;
+    /* set anchor point to center of element for transition */
+    transform-origin: center center 0 0 0 0;
+    transition-property: all;
+    transition-duration: 500ms;
+    text-shadow: 0px 0px 70px rgba(0, 0, 0, 0.352);
+  }
 
+  h1:hover {
+    /* on hover, change font size */
+    font-size: 50.2px;
+    text-shadow: 0px 0px 70px rgba(0, 0, 0, 0.932);
+    letter-spacing: .5px;
+  }
+
+  .button_icon {
+    margin: 0px;
+    margin-left: 1px;
+    margin-top: 5px;
+  }
+
+  .button {
+    display: inline;
+    background-color: rgba(0,0,0,.3);
+    color: white;
+    border-radius: 50%;
+    border: none;
+    height: 50px;
+    width: 50px;
+    margin-left: 20px;
+    margin-top: 15px;
+    transition-property: all;
+    transition-duration: 500ms;
+    -webkit-user-select: none; /* Safari */
+    -ms-user-select: none; /* IE 10 and IE 11 */
+    user-select: none; /* Standard syntax */
+    box-shadow: rgba(0, 0, 0, 0.11) 0px 0px 10px;
   }
 </style>
