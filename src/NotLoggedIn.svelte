53bf8a05b16c2274df90ac820cf96fe599a71067
IoyoCode <me@ioyo.dev>
2023-03-02 17:08:58 +0000

    firebase

--- src/NotLoggedIn.svelte
+++ src/NotLoggedIn.svelte
@@ -1,7 +1,7 @@
 <main>
     <div class="center">
         <h1>Please log in</h1>
-        <a class="withdiscord" href="https://discord.com/api/oauth2/authorize?client_id=1079489475165573170&redirect_uri=http%3A%2F%2F127.0.0.1%3A5173%2F&response_type=token&scope=identify%20email">
+        <a class="withdiscord" href="https://discord.com/api/oauth2/authorize?client_id=1079489475165573170&redirect_uri=https%3A%2F%2Fmemolink.info&response_type=token&scope=identify%20email">
             Discord
         </a>
     </div>
@@ -11,7 +11,7 @@
     @import url("https://fonts.googleapis.com/css2?family=Poppins&display=swap");
     main {
         width: 100%;
-        height: 100%;
+        height: 50%;
         padding: 0px;
         margin: 0px;
         background: linear-gradient(0deg, rgba(224,173,221,1) 0%, rgba(147,123,255,1) 2%, rgba(134,255,246,1) 100%);
